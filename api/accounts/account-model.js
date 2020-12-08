8ade1b5c91559096e0f108966eae89932d240320
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-07 21:21:16 -0500

    POST and PUT functional

--- api/accounts/account-model.js
+++ api/accounts/account-model.js
@@ -1,10 +1,23 @@
 const db = require("./../../data/dbConfig");
 
 module.exports = {
-    getAll() {
-        return db("accounts");
-    },
-    getById(id) {
-        return db("accounts").where({ id }).first();
-    }
-}
+  getAll() {
+    return db("accounts");
+  },
+  getById(id) {
+    return db("accounts").where({ id }).first();
+  },
+  create(account) {
+    return db("accounts")
+      .insert(account)
+      .then(([id]) => {
+        return db("accounts").where("id", id);
+      });
+  },
+  update(id, changes) {
+    return db("accounts").where("id", id).update(changes);
+  },
+  delete(id) {
+    return db("accounts").where("id", id).del();
+  },
+};
--- api/accounts/account-router.js
+++ api/accounts/account-router.js
@@ -11,4 +11,70 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Account.getById(req.params.id);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const validateAccount = (req, res, next) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Account body required" });
+  } else if (!req.body.name || !req.body.budget) {
+    res.status(400).json({ message: "name and budget are required fields" });
+  } else {
+    next();
+  }
+};
+
+router.post("/", validateAccount, async (req, res) => {
+  try {
+    const account = req.body;
+    const data = await Account.create(account);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const validateAccountId = async (req, res, next) => {
+  try {
+    const data = await Account.getById(req.params.id);
+    if (!data) {
+      res.status(404).json({ message: "Invalid id" })
+    } else {
+      next();
+    }
+ } catch {
+    res.status(500).json({ message: "Server error finding account" });
+ }
+  
+}
+
+router.put("/:id", validateAccountId, validateAccount, async (req, res) => {
+  try {
+    const changes = req.body;
+    const { id } = req.params;
+    const data = await Account.update(id, changes);
+    const updatedAccount = await Account.getById(id);
+    if(data < 1) {
+      res.status(500).json({ message: "Server Error" });
+    } else {
+      res.status(201).json(updatedAccount);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
